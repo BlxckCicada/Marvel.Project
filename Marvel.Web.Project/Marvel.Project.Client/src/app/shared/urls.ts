9f74236bf9cbbd64e12551f7f8e398a5eadb56af
Bafedile <Ddimakatso30@gmail.com>
2023-10-15 10:53:55 +0200

    updated

--- Marvel.Web.Project/Marvel.Project.Client/src/app/shared/urls.ts
+++ Marvel.Web.Project/Marvel.Project.Client/src/app/shared/urls.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UrlClass {
-  public static client_url = 'https://mcu-api.azurewebsites.net';
+  public static client_url = 'https://marvelmcu-api.azurewebsites.net';
   // public static client_url = '/Marvel.Project.Api';
 }
